861e9beb7911b0a09db7e4caf7a502aac114a887
jotcodeofficial <admin@jotcode.com>
2022-09-08 16:06:56 +0100

    added first snippet

---
+++ snippets/match.rs
@@ -0,0 +1,14 @@
+/// title: match
+/// tags: match
+
+fn match_snippet() {
+    let valid = true;
+    let amount = 3;
+    
+    let result = match (valid, amount) {
+        (valid, amount) if valid && amount == 3 => "valid",
+         _ => "invalid",
+    };
+    
+    println!("{}", result);
+}
