b7756e05427556e28a65d610924395d7a4d731d0
jotcodeofficial <admin@jotcode.com>
2022-09-08 18:10:38 +0100

    added binary hex octal

---
+++ snippets/binary_hex_octal.rs
@@ -0,0 +1,12 @@
+/// title: binary hex octal
+/// tags: binary hex octal hexadecimal 
+
+
+fn binary_hex_octal() {
+    let number: usize = 536;
+
+    println!(
+        "number: {},\nbinary: {:b},\nhex: {:x},\noctal: {:o}",
+        number, number, number, number
+    );
+}
