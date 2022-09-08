4a068b46d9e1fbf502a0b3b9c99ea7770d284c0b
jotcodeofficial <admin@jotcode.com>
2022-09-08 18:02:09 +0100

    char amount and pointer address snippets

---
+++ snippets/char_amount.rs
@@ -0,0 +1,8 @@
+/// title: char amount
+/// tags: char, chars, count
+
+fn char_amount() {
+    let name = "jotcode";
+
+    println!("chars amount: {}", name.chars().count());
+}
---
+++ snippets/pointer_address.rs
@@ -0,0 +1,9 @@
+fn pointer_address() {
+    let name = "jotcode";
+    let reference_value = &vec![1, 2, 3];
+
+    println!(
+        "pointer address for &str: {:p}\npointer address for vec pointer: {:p}",
+        name, reference_value
+    );
+}
