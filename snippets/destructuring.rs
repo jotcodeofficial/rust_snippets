7f7385530ee6e71862ae387e0de9aeab4c12e2c1
jotcodeofficial <admin@jotcode.com>
2022-09-12 10:48:46 +0100

    destructing update

--- snippets/destructuring.rs
+++ snippets/destructuring.rs
@@ -5,20 +5,23 @@
 struct Monster {
     name: String,
     age: u8,
+    color: String,
 }
 
 fn destructuring() {
     let rawr = Monster {
         name: "rawr".to_string(),
         age: 8,
+        color: "red".to_string(),
     };
     
     println!("{:#?}", rawr);
     
     let Monster {
-        name,
+        name: n,
         age,
+        ..
     } = rawr;
     
-    println!("{} and {}", name, age);
+    println!("{} and {}", n, age);
 }
