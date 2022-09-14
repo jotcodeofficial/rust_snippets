d4a1afc66ffdfc78b9bc40684b5a3375e1592c36
jotcodeofficial <admin@jotcode.com>
2022-09-14 10:59:26 +0100

    option and expect

---
+++ snippets/option_and_expect.rs
@@ -0,0 +1,19 @@
+
+fn grab_last_if_bigger_than_3(list: Vec<i32>) -> Option<i32>{
+    if list.len() > 3 {
+        Some(list[list.len() - 1])
+    } else {
+        None
+    }
+}
+
+fn option_and_expect() {
+    let list_1 = vec![1, 2, 3];
+    let list_2 = vec![1, 2, 3, 4, 5, 6];
+    
+    let result_1 = grab_last_if_bigger_than_3(list_1);
+    let result_2 = grab_last_if_bigger_than_3(list_2);
+    
+    println!("{:?}", result_1.expect("The list is not bigger than 3"));
+    println!("{:?}", result_2.expect("The list is not bigger than 3"))
+}
